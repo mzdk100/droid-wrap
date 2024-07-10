5d8dfef89019522597914bd1c7289d5e1f71ac0c
SmileSky <mzdk100@foxmail.com>
2024-07-10 08:27:48 +0800

    发布0.2.3版本。

--- publish.cmd
+++ publish.cmd
@@ -1,6 +1,6 @@
 @echo off
-cargo doc --target aarch64-linux-android
-cargo test --doc --target aarch64-linux-android
+cargo doc --target aarch64-linux-android --all-features
+cargo test --doc --target aarch64-linux-android --all-features
 cargo publish -p droid-wrap-derive --all-features
 cargo publish -p droid-wrap-utils --all-features --target aarch64-linux-android
 cargo publish --all-features --target aarch64-linux-android
