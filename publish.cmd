7497adc06d9bd9637cf3a3bd40ef5777f5340d42
Administrator <mzdk100@foxmail.com>
2025-02-26 16:58:21 +0800

    Fix publish cmd to publish droid-wrap-macros

--- publish.cmd
+++ publish.cmd
@@ -1,6 +1,6 @@
 @echo off
 cargo doc --all-features &&^
-cargo publish -p droid-wrap-derive --all-features &&^
+cargo publish -p droid-wrap-macros --all-features &&^
 cargo publish -p droid-wrap-utils --all-features &&^
 cargo publish --all-features
 pause
